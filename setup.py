a5ca2b86491e66a6d5fd4397c6f3d19042dc34ea
enricobacis <enrico.bacis@gmail.com>
2017-02-12 17:47:47 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('README.rst') as README:
+    long_description = README.read()
+    long_description = long_description[long_description.index('Description'):]
+
+setup(name='limit',
+      version='0.1',
+      description='Decorator that limits the calling rate of functions',
+      long_description=long_description,
+      url='http://github.com/enricobacis/limit',
+      author='Enrico Bacis',
+      author_email='enrico.bacis@gmail.com',
+      license='MIT',
+      packages=['limit'],
+      keywords='rate ratelimit ratelimiter limit limiting api function'
+)
